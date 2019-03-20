957be2dbc6d75a6fa456e9f8b3aab64097827c71
Denis <vite0150@gmail.com>
2019-03-19 19:37:46 -0600

    Actualizacion a android 9

--- Gestion_Academica/app/src/main/java/com/gestion_academica/Variables.java
+++ Gestion_Academica/app/src/main/java/com/gestion_academica/Variables.java
@@ -9,7 +9,7 @@ public class Variables extends Application{
 
 //    URL base del backend
 
-    private static final String URLBase = "http://192.168.2.175:8084/UniversidadBackend/AndroidServlet?"; // Cambiarla segun donde se ejecute el backend
+    private static final String URLBase = "http://192.168.1.3:8084/UniversidadBackend/AndroidServlet?"; // Cambiarla segun donde se ejecute el backend
 
 
     public static String getURLBase() {
